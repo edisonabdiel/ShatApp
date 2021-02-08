c078613984256ea7a3efc0effe912fbe59ecb317
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-02-08 21:51:10 +0100

    Create ChatFeed MessageForm MyMessage TheirMessage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const projectID = 'c8b7c520-5773-4ae3-a13f-81020068acc9';
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID={projectID}
+            userName="zombiotch"
+            userSecret="notasecret"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+    )
+};
 
 export default App;
